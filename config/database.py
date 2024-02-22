12b1a3427ac1101d1fbfc6fa3967ff20e13af86a
Ivan Martinez Cañero <ivanmartinezca10@gmail.com>
2024-02-22 20:15:59 +0100

    Merge testing

--- config/database.py
+++ config/database.py
@@ -5,7 +5,7 @@ from dotenv import dotenv_values
 
 secrets = dotenv_values(".env")
 
-uri = f"mongodb+srv://{secrets["DB_USER"]}:{secrets["DB_PASS"]}@ecommercecluster.ubewfnb.mongodb.net/?retryWrites=true&w=majority"
+uri = f"mongodb+srv://{secrets['DB_USER']}:{secrets['DB_PASS']}@ecommercecluster.ubewfnb.mongodb.net/?retryWrites=true&w=majority"
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
